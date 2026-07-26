dbd277f131ce23bf611dcd47173d969fb5f3d3d3
ConsultoParis <guillaumehaddad@hotmail.com>
2026-07-26 23:30:54 +0200

    Update Stripe API version to 2025-02-24.acacia

--- lib/stripe-featured.ts
+++ lib/stripe-featured.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripeFeatured = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export const FEATURED_PRICE_ID = process.env.STRIPE_FEATURED_PRICE_ID!;
